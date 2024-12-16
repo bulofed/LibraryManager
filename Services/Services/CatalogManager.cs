649e6f73d753521f994e7d185b57855765aec640
NoName-coding <hugohallot@gmail.com>
2024-12-16 17:20:12 +0100

    4.2

---
+++ Services/Services/CatalogManager.cs
@@ -0,0 +1,20 @@
+﻿using BusinessObjects.Entity;
+
+namespace Services.Services
+{
+    public class CatalogManager
+    {
+        public void GetCatalog()
+        {
+
+        }
+        public void GetCatalog(Type type)
+        {
+
+        }
+        public void FindBook(int id)
+        {
+
+        }
+    }
+}
