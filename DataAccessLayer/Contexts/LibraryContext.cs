7ef6ae4d5c60178e0357462b0da68933ddea2cff
NoName-coding <hugohallot@gmail.com>
2024-12-17 10:48:40 +0100

    feat(db connect (start))

---
+++ DataAccessLayer/Contexts/LibraryContext.cs
@@ -0,0 +1,14 @@
+
+using BusinessObjects.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLayer.Contexts;
+
+
+public class LibraryContext: DbContext
+{
+    public DbSet<Book> Books { get; set; }
+    public DbSet<Author> Authors { get; set; }
+    public DbSet<Library> Libraries { get; set; }
+    public DbSet<Stock> Stocks { get; set; }
+}
